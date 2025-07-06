fa3ce8b9b7c75c557a74e3ecf486d7a164dfeb67
Thejan Nimsara <thejan.n@azendtech.com>
2025-07-06 07:15:13 +0530

    Fix route issue in editor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,10 @@
-// "use client"
+import { redirect } from 'next/navigation';
 
-// import { useRouter } from "next/navigation";
-// import { useEffect } from "react";
+function generateDocumentId(): string {
+  return "cd4d247c-999a-4137-83e1-5b1d4103812b";
+}
 
-// export default function Home() {
-//   const router = useRouter();
-
-//   useEffect(() => {
-//     const dynamicDocId = localStorage.getItem('lastVisitedDocId') || 'editor/b910ea57-74c5-456d-b516-6bd5e6c28604'; // Replace with your actual logic
-
-//     if (dynamicDocId) {
-//       router.push(`/${dynamicDocId}`);
-//     }
-//   }, [router]);
-
-//   return <></>;
-// }
-
-// export async function getServerSideProps() {
-  // Generate a new document ID
-  // const generateDocumentId = () => {
-  //   return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-  // };
-
-  // const documentId = generateDocumentId();
-
-  // return {
-  //   redirect: {
-  //     destination: `/editor/${documentId}`,
-  //     permanent: false, // Set to true if this is a permanent redirect
-  //   },
-  // };
-// }
-
-// This component will never actually render due to the redirect
-export default async function HomePage() {
-  return <h1>HomePage</h1>;
+export default function HomePage() {
+  const documentId = generateDocumentId();
+  redirect(`/editor/${documentId}`);
 }
