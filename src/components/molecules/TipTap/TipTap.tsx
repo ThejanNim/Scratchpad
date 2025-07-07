9baac265a4b266209e583de44750e273344f2222
Thejan Nimsara <thejan.n@azendtech.com>
2025-07-07 08:47:10 +0530

    Add outline and resource part

---
+++ src/components/molecules/TipTap/TipTap.tsx
@@ -0,0 +1,70 @@
+'use client'
+
+import './styles.css'
+import { useEditor, EditorContent } from '@tiptap/react'
+import StarterKit from '@tiptap/starter-kit'
+import { EditorProvider, useCurrentEditor } from '@tiptap/react'
+import { Color } from '@tiptap/extension-color'
+import ListItem from '@tiptap/extension-list-item'
+import TextStyle from '@tiptap/extension-text-style'
+
+const MenuBar = () => {
+  const { editor } = useCurrentEditor()
+
+  if (!editor) {
+    return null
+  }
+
+  return (
+    <div className="control-group">
+      <div className="button-group">
+        {/* <button
+          onClick={() => {editor.chain().focus().toggleBulletList().run()
+            console.log("Bullet")
+          }}
+          className={editor.isActive('bulletList') ? 'is-active' : ''}
+        >
+          Bullet list
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleOrderedList().run()}
+          className={editor.isActive('orderedList') ? 'is-active' : ''}
+        >
+          Ordered list
+        </button> */}
+      </div>
+    </div>
+  )
+}
+
+const extensions = [
+  Color.configure({ types: [TextStyle.name, ListItem.name] }),
+  TextStyle.configure({ types: [ListItem.name] }),
+  StarterKit.configure({
+    bulletList: {
+      keepMarks: true,
+      keepAttributes: false, // TODO : Making this as `false` becase marks are not preserved when I try to preserve attrs, awaiting a bit of help
+    },
+    orderedList: {
+      keepMarks: true,
+      keepAttributes: false, // TODO : Making this as `false` becase marks are not preserved when I try to preserve attrs, awaiting a bit of help
+    },
+  }),
+]
+
+const content = `
+<ul>
+  <li>
+    First outline
+  </li>
+</ul>
+`
+
+export default function TipTap() {
+  // const editor = useEditor({
+  //   extensions: [StarterKit],
+  //   content: '<p>Hello World! 🌎️</p>',
+  // })
+
+  return <EditorProvider slotBefore={<MenuBar />} extensions={extensions} content={content}></EditorProvider>
+}
