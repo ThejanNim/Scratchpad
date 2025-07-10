4e401b537c3622a55d4b7a1313f50da6209fe55d
Thejan Nimsara <thejan.n@azendtech.com>
2025-07-10 19:05:08 +0530

    refactor: remove unwanted Promise from dynamic routes

--- src/app/editor/[document]/page.tsx
+++ src/app/editor/[document]/page.tsx
@@ -3,9 +3,7 @@ import DocumentTemplate from "@/components/templates/DocumentTemplate";
 export default async function Document({
   params,
 }: {
-  params: Promise<{ document: string }>;
+  params: { document: string }
 }) {
-  const { document } = await params;
-    
-  return <DocumentTemplate documentId={document} />
+  return <DocumentTemplate documentId={params.document} />
 }
